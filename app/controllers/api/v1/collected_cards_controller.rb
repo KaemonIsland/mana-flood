217e7e50f20b020eae8b689d9855f7578730f8b7
Kaemon Lovendahl <k-manskrazy@hotmail.com>
2020-02-17 14:38:21 -0700

    Move collection crud actions to API

--- app/controllers/api/v1/collected_cards_controller.rb
+++ app/controllers/api/v1/collected_cards_controller.rb
@@ -1,19 +1,66 @@
 class Api::V1::CollectedCardsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :load_card, :load_collection, only: [:create, :update, :destroy]
+  before_action :load_collected_card, only: [:update, :destroy]
   respond_to :json
 
   def index
     if !current_user
-      @collection = current_user.collection.collected_cards
-      @collection_cards = current_user.collection.cards
+      @collection = User.first.collection.collected_cards
+      @collection_cards = User.first.collection.cards
       render json: { collection: @collection, collection_cards: @collection_cards }
     else
       render json: { error: 'User must be signed in' }, status: 401
     end
   end
+  
+    def create
+      if @collection.cards.find(@card.id)
+        render json: { error: 'Unable to add card to collection' }, status: 400
+      elsif @collection.cards << @card
+        @collected_card = @collection.collected_cards.find_by(card_id: @card.id)
+        @collected_card.quantity = 1
+        @collected_card.save
 
-  def add_card
-  end
+        render json: { collection: @collection.collected_cards, collection_cards: @collection.cards }, status: 201
+      else
+        render json: { error: 'Unable to add card to collection' }, status: 400
+      end
+    end
+  
+    def update
+      if @collected_card.update(collected_card_params)
+        render json: { collection: @collection.collected_cards, collection_cards: @collection.cards }, status: 200
+      else
+        render json: { error: 'Unable to update card quantity' }, status: 400
+      end
+    end
+  
+    def destroy
+      if !@collected_card 
+        render json: { error: 'Card not found' }, status: 404
+      elsif @collected_card.destroy
+        render json: { collection: @collection.collected_cards, collection_cards: @collection.cards }, status: 200
+      else
+        render json: { error: 'Unable to remove card from collection' }, status: 400
+      end
+    end
+  
+    private
+  
+    def load_card
+      @card = Card.find(params[:id])
+    end
+  
+    def load_collection
+      @collection = User.first.collection
+    end
+  
+    def load_collected_card
+      @collected_card = @collection.collected_cards.find_by(card_id: @card.id)
+    end
 
-  def remove_card
+    def collected_card_params
+      params.permit(:quantity)
+    end
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      # Get user collection, and view card sets
       get 'collection', to: 'collected_cards#index'
       get 'card_sets', to: 'card_sets#index'
       get 'card_set/:id', to: 'card_sets#show'
+
+      # Crud operations for users card collection
+      post 'add_card/:id', to: 'collected_cards#create', as: 'add_card'
+      put 'add_card/:id', to: 'collected_cards#update', as: 'update_card'
+      delete 'remove_card/:id', to: 'collected_cards#destroy', as: 'remove_card'
     end
   end
 
@@ -11,9 +17,6 @@ Rails.application.routes.draw do
 
   get 'sets', to: 'card_sets#index'
   get 'sets/:id', to: 'card_sets#show'
-  post 'add_card/:id', to: 'collected_cards#create', as: 'add_card'
-  put 'add_card/:id', to: 'collected_cards#update', as: 'update_card'
-  delete 'remove_card/:id', to: 'collected_cards#destroy', as: 'remove_card'
 
   post 'update_cards', to: 'cards#update_card_db'
   post 'update_card_sets', to: 'card_sets#update_card_set_db'
