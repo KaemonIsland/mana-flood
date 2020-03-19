2b3dd20a809fe1ee0f7c32bd52cb7192567a5db6
KaemonIsland <k-manskrazy@hotmail.com>
2020-03-18 19:02:53 -0600

    Add formatDate util

---
+++ app/javascript/app/src/utils/formatDate.ts
@@ -0,0 +1,15 @@
+type Options = {
+  weekday?: string
+  year?: string
+  month?: string
+  day?: string
+}
+
+/**
+ * Formats the date using toLocaleDatestring.
+ * accepts options for weekay, year, month and day.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+ */
+export const formatDate = (date: Date, dateOptions: Options): string =>
+  date.toLocaleDateString(undefined, dateOptions)
