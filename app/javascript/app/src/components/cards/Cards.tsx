ca6dad320f56031279ccd1eeb74fca568d66cede
KaemonIsland <k-manskrazy@hotmail.com>
2020-05-19 19:58:45 -0600

    Add filters for type rarity and cmc

--- app/javascript/app/src/components/cards/Cards.tsx
+++ app/javascript/app/src/components/cards/Cards.tsx
@@ -26,8 +26,8 @@ const StyledGrid = styled.div`
 const FilterContent = styled.div(({ theme }) => ({
   backgroundColor: theme.color.grey[3],
   borderRight: '1px solid black',
-  height: '100vh',
-  overflowY: 'hidden',
+  height: '100%',
+  overflowY: 'scroll',
   padding: theme.spaceScale(4),
 }))
 
@@ -36,10 +36,13 @@ const FilterBox = styled.div(({ theme }) => ({
   borderRadius: theme.spaceScale(2),
   border: '1px solid black',
   padding: theme.spaceScale(1),
+  marginBottom: '1rem',
 }))
 
 export const Cards = ({ actions, cards }) => {
   const { filteredCards, filters, updateFilters } = useFilter(cards)
+
+  console.log(filteredCards)
 
   const { color, type, rarity, cmc } = filters
 
@@ -56,8 +59,10 @@ export const Cards = ({ actions, cards }) => {
             <div>
               <input
                 type="checkbox"
+                name="color"
                 id="all"
                 value="all"
+                onChange={updateFilters}
                 checked={color.length === 0}
               />
               <label htmlFor="all">All</label>
@@ -69,6 +74,7 @@ export const Cards = ({ actions, cards }) => {
                 id="white"
                 value="W"
                 onChange={updateFilters}
+                checked={color.includes('W')}
               />
               <label htmlFor="white">White</label>
             </div>
@@ -79,6 +85,7 @@ export const Cards = ({ actions, cards }) => {
                 id="blue"
                 onChange={updateFilters}
                 value="U"
+                checked={color.includes('U')}
               />
               <label htmlFor="blue">Blue</label>
             </div>
@@ -88,6 +95,7 @@ export const Cards = ({ actions, cards }) => {
                 name="color"
                 id="black"
                 onChange={updateFilters}
+                checked={color.includes('B')}
                 value="B"
               />
               <label htmlFor="black">Black</label>
@@ -98,6 +106,7 @@ export const Cards = ({ actions, cards }) => {
                 name="color"
                 id="red"
                 onChange={updateFilters}
+                checked={color.includes('R')}
                 value="R"
               />
               <label htmlFor="red">Red</label>
@@ -109,6 +118,7 @@ export const Cards = ({ actions, cards }) => {
                 id="green"
                 onChange={updateFilters}
                 value="G"
+                checked={color.includes('G')}
               />
               <label htmlFor="green">Green</label>
             </div>
@@ -119,14 +129,189 @@ export const Cards = ({ actions, cards }) => {
                 id="colorless"
                 onChange={updateFilters}
                 value="C"
+                checked={color.includes('C')}
               />
               <label htmlFor="colorless">Colorless</label>
             </div>
           </Flex>
         </FilterBox>
-        <FilterBox>CMC</FilterBox>
-        <FilterBox>Type</FilterBox>
-        <FilterBox>Rarity</FilterBox>
+        <FilterBox>
+          <Flex alignItems="center" justifyContent="space-between">
+            <div>Type</div>
+            <div>-</div>
+          </Flex>
+          <hr />
+          <Flex direction="column" alignItems="start" justifyContent="start">
+            <div>
+              <input
+                type="radio"
+                name="type"
+                id="all"
+                value="all"
+                onChange={updateFilters}
+                defaultChecked
+              />
+              <label htmlFor="all">All</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="type"
+                id="creature"
+                value="creature"
+                onChange={updateFilters}
+              />
+              <label htmlFor="creature">Creature</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="type"
+                id="sorcery"
+                onChange={updateFilters}
+                value="sorcery"
+              />
+              <label htmlFor="sorcery">Sorcery</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="type"
+                id="instant"
+                onChange={updateFilters}
+                value="instant"
+              />
+              <label htmlFor="instant">Instant</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="type"
+                id="enchantment"
+                onChange={updateFilters}
+                value="enchantment"
+              />
+              <label htmlFor="enchantment">Enchantment</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="type"
+                id="artifact"
+                onChange={updateFilters}
+                value="artifact"
+              />
+              <label htmlFor="artifact">Artifact</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="type"
+                id="planeswalker"
+                onChange={updateFilters}
+                value="planeswalker"
+              />
+              <label htmlFor="planeswalker">Planeswalker</label>
+            </div>
+          </Flex>
+        </FilterBox>
+        <FilterBox>
+          <Flex alignItems="center" justifyContent="space-between">
+            <div>Rarity</div>
+            <div>-</div>
+          </Flex>
+          <hr />
+          <Flex direction="column" alignItems="start" justifyContent="start">
+            <div>
+              <input
+                type="checkbox"
+                name="rarity"
+                id="all"
+                value="all"
+                onChange={updateFilters}
+                checked={rarity.length === 0}
+              />
+              <label htmlFor="all">All</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="rarity"
+                id="common"
+                value="common"
+                onChange={updateFilters}
+                checked={rarity.includes('common')}
+              />
+              <label htmlFor="common">Common</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="rarity"
+                id="uncommon"
+                onChange={updateFilters}
+                value="uncommon"
+                checked={rarity.includes('uncommon')}
+              />
+              <label htmlFor="uncommon">Uncommon</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="rarity"
+                id="Rare"
+                onChange={updateFilters}
+                checked={rarity.includes('rare')}
+                value="rare"
+              />
+              <label htmlFor="Rare">Rare</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="rarity"
+                id="mythic"
+                onChange={updateFilters}
+                checked={rarity.includes('mythic')}
+                value="mythic"
+              />
+              <label htmlFor="mythic">Mythic</label>
+            </div>
+          </Flex>
+        </FilterBox>
+        <FilterBox>
+          <Flex alignItems="center" justifyContent="space-between">
+            <div>CMC</div>
+            <div>-</div>
+          </Flex>
+          <hr />
+          <Flex direction="column" alignItems="start" justifyContent="start">
+            <div>
+              <label htmlFor="min">Min</label>
+              <input
+                type="number"
+                name="min"
+                min={0}
+                max={20}
+                id="min"
+                value={cmc.min}
+                onChange={updateFilters}
+              />
+            </div>
+            <div>
+              <label htmlFor="max">Max</label>
+              <input
+                type="number"
+                name="max"
+                min={0}
+                max={20}
+                value={cmc.max}
+                id="max"
+                onChange={updateFilters}
+              />
+            </div>
+          </Flex>
+        </FilterBox>
       </FilterContent>
       <StyledGrid>
         {filteredCards.map(card => (
--- app/javascript/app/src/utils/hooks/useFilter.tsx
+++ app/javascript/app/src/utils/hooks/useFilter.tsx
@@ -5,24 +5,82 @@ export const useFilter = cards => {
   const [filters, setFilters] = useState({
     color: [],
     rarity: [],
-    type: [],
-    cmc: null,
+    type: null,
+    cmc: {
+      min: 0,
+      max: 20,
+    },
   })
 
-  const { color, type, rarity, cmc } = filters
+  const { color, type, rarity } = filters
+  const { min, max } = filters.cmc
 
-  const updateFilters = ({ target }) => {
-    const { name, value } = target
-
+  /**
+   * Updates an array of options, either adding a new item to the array, or removing it.
+   *
+   * @param {string} name - category of the filter
+   * @param {string} value - value to add/remove from filter category
+   */
+  const updateMultiple = ({ name, value }) => {
     const filter = filters[name]
 
-    if (filter.includes(value)) {
-      setFilters({ ...filters, [name]: filter.filter(val => val !== value) })
+    if (value === 'all') {
+      setFilters({ ...filters, [name]: [] })
+    } else if (filter.includes(value)) {
+      setFilters({
+        ...filters,
+        [name]: filter.filter(val => val !== value),
+      })
     } else {
       setFilters({ ...filters, [name]: [...filter, value] })
     }
   }
 
+  /**
+   * Updates a single value to the new value
+   *
+   * @param {string} name - category of the filter
+   * @param {string} value - value to add/remove from filter category
+   */
+  const updateSingle = ({ name, value }) => {
+    if (value === 'all') {
+      setFilters({ ...filters, [name]: null })
+    } else {
+      setFilters({ ...filters, [name]: value })
+    }
+  }
+
+  /**
+   * Updates a range of values, these include a min/max value
+   *
+   * @param {string} name - category of the filter
+   * @param {string} value - value to add/remove from filter category
+   */
+  const updateRange = ({ name, value }) => {
+    setFilters({
+      ...filters,
+      cmc: { ...filters.cmc, [name]: value },
+    })
+  }
+
+  /**
+   * Decides how to update our various filter
+   *
+   * @param {object} target - value form form element
+   */
+  const updateFilters = ({ target }) => {
+    const { name } = target
+
+    if (name === 'color' || name === 'rarity') {
+      updateMultiple(target)
+    } else if (name === 'type') {
+      updateSingle(target)
+    } else if (name === 'min' || name === 'max') {
+      updateRange(target)
+    }
+  }
+
+  // Filters cards by color
   if (color.length !== 0) {
     filteredCards = filteredCards.filter(card => {
       if (card.colors.length === 0 && color.includes('C')) {
@@ -30,6 +88,36 @@ export const useFilter = cards => {
       }
       return card.colors.some(cardColor => color.includes(cardColor))
     })
+  }
+
+  // Filters cards by rarity
+  if (rarity.length !== 0) {
+    filteredCards = filteredCards.filter(card =>
+      rarity.some(rare => card.rarity === rare)
+    )
+  }
+
+  // Filters cards by type
+  if (type) {
+    filteredCards = filteredCards.filter(card =>
+      JSON.parse(card.card_types).some(cardType =>
+        type.includes(cardType.toLowerCase())
+      )
+    )
+  }
+
+  // Filters cards by minimum converted mana cost
+  if (min) {
+    filteredCards = filteredCards.filter(
+      card => card.converted_mana_cost >= min
+    )
+  }
+
+  // Filters cards by maximum converted mana cost
+  if (max) {
+    filteredCards = filteredCards.filter(
+      card => card.converted_mana_cost <= max
+    )
   }
 
   return {
